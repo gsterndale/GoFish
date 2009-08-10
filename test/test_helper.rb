10b8e60b9869161e08478ba52de9dfb05be0efbd
Greg <greg@plectix.com>
2009-08-09 21:33:49 -0400

    Require Ruby Debug in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'test/unit'
 require 'mocha'
+require 'ruby-debug'
 
 require File.dirname(__FILE__) + '/../go_fish'
 require File.dirname(__FILE__) + '/../fishing_forecaster'
