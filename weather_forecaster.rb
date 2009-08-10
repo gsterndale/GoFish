e8fb1f57819f25c182cd5596d2624d86478a0b58
Greg <greg@plectix.com>
2009-08-09 21:34:20 -0400

    WeatherForecaster finds likelihood_of_rain

--- weather_forecaster.rb
+++ weather_forecaster.rb
@@ -1,3 +1,42 @@
+require 'rubygems'
+require 'uri'
+require 'open-uri'
+require 'hpricot'
+
+class ServiceError < RuntimeError; end
+
 class WeatherForecaster
+  
+  # Thanks Umbrella Today!
+  def self.likelihood_of_rain
+    doc = Hpricot::XML(weather_xml)
+    pop = (doc/'probability-of-precipitation'/'value').first.innerHTML.to_f
+    pop / 100.0
+  rescue Timeout::Error, 
+         Errno::EINVAL, 
+         Errno::ECONNRESET, 
+         EOFError,
+         Net::HTTPBadResponse, 
+         Net::HTTPHeaderSyntaxError,
+         Net::ProtocolError => e
+    service_error = ServiceError.new
+    service_error.set_backtrace(e.backtrace)
+    raise service_error
+  end
+
+protected
+
+  def self.weather_xml
+    service_uri.read
+  end
+  
+  def self.service_uri
+    URI.parse(service_url)
+  end
+  
+  def self.service_url
+    time = Time.now.strftime("%Y-%m-%d")
+    "http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?&format=24+hourly&numDays=2&lat=42.34577&lon=-71.073875&startDate=#{time}"
+  end
   
 end
